ff312d8388bfc1e0e9592b5f3f01637e4f4adf48
José Luan Lima da Costa <54694573+jose-luan19@users.noreply.github.com>
2020-05-26 18:27:02 -0300

    Create area.java

---
+++ fundaments/area.java
@@ -0,0 +1,9 @@
+public class area {
+	public static void main(String[] args) {
+		double raio = 3.2;
+		final double PI = 3.14159;
+		double area = PI*raio*raio;
+		System.out.println("Area: "+area+"m2");	
+	}
+	
+}
